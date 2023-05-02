86bb0ee02a79d5582ad56812e7bd099e703b36f0
GrantWelsh27 <grantwwork@gmail.com>
2023-05-02 08:43:31 -0600

    Menu

--- src/MyPkg/Main.java
+++ src/MyPkg/Main.java
@@ -1,14 +1,32 @@
 package MyPkg;
-
+import java.util.Objects;
+import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Please let this work");
-        System.out.println("Let's go!!!");
-        System.out.println("I hate GitHub");
-        System.out.println("I just wrote this from the github website");
+        System.out.println("Welcome to the Run-O-Matic 5000");
+        while(true) {
+            System.out.println("Please Enter the function you would like to use");
+            System.out.print("1- Race Time Predictor\t\t2- Split Finder\n3- Pace Calculator\t\t\t4- Elevation Guru\n5- Speed Converter\t\t\t6- Function Info\n7- Exit\n");
+            System.out.print("||~ ");
+            int userFunction = input.nextInt();
 
-        SpeedConverter s = new SpeedConverter(60, 100);
+            switch (userFunction) {
+                case 1 -> new RacePredictor();
+                case 2 -> new SplitFinder();
+                case 3 -> new RacingPaceCalculator();
+                case 4 -> new ElevationGuru();
+                case 5 -> new SpeedConverter();
+                case 6 -> System.out.print(FunctionInfo());
+                case 7 -> System.exit(69);
+            }
+        }
 
+
+    }
+
+    private static String FunctionInfo() {
+        return "shorts\nEnter to continue";
     }
 }
