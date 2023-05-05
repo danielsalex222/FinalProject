6f902c79cc5cab019e89bb4a7ff77a3eb16c7c63
danielsalex222 <127963331+danielsalex222@users.noreply.github.com>
2023-05-05 09:48:08 -0600

    Not sure if this will work but here...

--- src/MyPkg/RacePredictor.java
+++ src/MyPkg/RacePredictor.java
@@ -7,19 +7,27 @@ public class RacePredictor extends TimeDistanceCalculator{
     }
 
     public String calculate() {
-        return "I'm a race predictor";
+        int[] XD = conversion(distance, minutes, seconds);
+        StringBuilder p = new StringBuilder();
+        p.append.String.format("The converted mile time is: %d:%d", XD[1]/60, XD[1]%60);
+        p.append.String.format("The converted 2 mile time is: %d:%d", XD[2]/60, XD[2]%60);
+        p.append.String.format("The converted 5K time is: %d:%d", XD[3]/60, XD[3]%60);
+        p.append.String.format("The converted 10K time is: %d:%d", XD[4]/60, XD[4]%60);
+        return p.toString()//"I'm a race predictor";
     }
 
-    public double conversion(int d, int m, int s) {
+    public static int[] conversion(int d, int m, int s) {
         
         int seconds = m*60 + s;
 
         double mile = seconds * (Math.pow((1609 / d), 1.06));
-        double 2mile = seconds * (Math.pow((1609 / d), 1.06));
-        double FiveK = seconds * (Math.pow((1609 / d), 1.06));
-        double TenK = seconds * (Math.pow((1609 / d), 1.06));
+        double 2mile = seconds * (Math.pow((3218 / d), 1.06));
+        double FiveK = seconds * (Math.pow((5000 / d), 1.06));
+        double TenK = seconds * (Math.pow((10000 / d), 1.06));
 
-        return 1;
+        int[] ConvertedTimes = {mile, 2mile, FiveK, TenK};
+
+        return ConvertedTimes;
 
     }
 
