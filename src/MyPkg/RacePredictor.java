0f74cedd630269d167d124f7e6025e5935c5f368
danielsalex222 <127963331+danielsalex222@users.noreply.github.com>
2023-05-05 08:42:35 -0600

    Conversion!

--- src/MyPkg/RacePredictor.java
+++ src/MyPkg/RacePredictor.java
@@ -10,4 +10,17 @@ public class RacePredictor extends TimeDistanceCalculator{
         return "I'm a race predictor";
     }
 
+    public double conversion(int d, int m, int s) {
+        
+        int seconds = m*60 + s;
+
+        double mile = seconds * (Math.pow((1609 / d), 1.06));
+        double 2mile = seconds * (Math.pow((1609 / d), 1.06));
+        double FiveK = seconds * (Math.pow((1609 / d), 1.06));
+        double TenK = seconds * (Math.pow((1609 / d), 1.06));
+
+        return 1;
+
+    }
+
 }
